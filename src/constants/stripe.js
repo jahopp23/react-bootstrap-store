aa4001274c7a010e87b2593a9d54bfaf4075b29e
John A. Hopper <jahopp@gmail.com>
2018-05-30 19:37:01 -0500

    stripe publishable

--- src/constants/stripe.js
+++ src/constants/stripe.js
@@ -1,6 +1,6 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'development'
-  ? 'pk_test_ZjR89CTSXjbX8aSLB4iJz1qM'
-  : 'sk_test_u1YwL5Xxcxbvh7gz1MIbI5tY';
+  ? 'pk_test_STRIPE_KEY'
+  : 'sk_test_STRIPE_KEY';
 
 
 export default STRIPE_PUBLISHABLE;
